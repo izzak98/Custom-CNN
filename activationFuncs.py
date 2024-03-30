5fe070c640ce8f03484ff3cba422ddffce079230
izzak98 <izzak98@hotmail.com>
2024-03-30 13:32:41 +0000

    Add activation functions and their derivatives

---
+++ activationFuncs.py
@@ -0,0 +1,44 @@
+import cupy as np
+from cupy import ndarray
+
+
+def relu(x: ndarray) -> ndarray:
+    return np.maximum(0, x)
+
+
+def relu_derivative(x: ndarray) -> ndarray:
+    return np.where(x > 0, 1, 0)
+
+
+def sigmoid(x: ndarray) -> ndarray:
+    return 1 / (1 + np.exp(-x))
+
+
+def sigmoid_derivative(x: ndarray) -> ndarray:
+    return sigmoid(x) * (1 - sigmoid(x))
+
+
+def tanh(x: ndarray) -> ndarray:
+    return np.tanh(x)
+
+
+def tanh_derivative(x: ndarray) -> ndarray:
+    return 1 - np.tanh(x) ** 2
+
+
+def softmax(x: ndarray) -> ndarray:
+    exps = np.exp(x - np.max(x))
+    return exps / np.sum(exps)
+
+
+def softmax_derivative(x: ndarray) -> ndarray:
+    s = x.reshape(-1, 1)
+    return np.diagflat(s) - np.dot(s, s.T)
+
+
+act_derivative_mapping = {
+    relu: relu_derivative,
+    sigmoid: sigmoid_derivative,
+    tanh: tanh_derivative,
+    softmax: softmax_derivative
+}
