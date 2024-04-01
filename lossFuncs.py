ef7ed1d706ef5215b51ccaf4e8a55c99a4db64df
izzak98 <izzak98@hotmail.com>
2024-04-01 22:29:45 +0000

    Refactor loss functions to support sparse categorical crossentropy

--- lossFuncs.py
+++ lossFuncs.py
@@ -9,27 +9,20 @@ def mean_squared_error_derivative(y_true, y_pred):
     return 2 * (y_pred - y_true) / y_true.size
 
 
-def categorical_crossentropy(y_true, y_pred):
-    pred_vect = np.zeros_like(y_pred)
-    pred_vect[np.arange(y_true.size), y_true.astype(np.int_)] = 1
-    return -np.sum(pred_vect * np.log(y_pred + 1e-9)) / y_true.size
+def sparse_categorical_crossentropy(y_true, y_pred_logits):
+    n_samples = y_pred_logits.shape[0]
+    y_true_one_hot = np.eye(y_pred_logits.shape[1])[y_true.astype(int)]
+    loss = -np.sum(y_true_one_hot * np.log(y_pred_logits + 1e-15)) / n_samples
+    return loss
 
 
-def categorical_crossentropy_derivative(y_true, y_pred):
-    pred_vect = np.zeros_like(y_pred)
-    pred_vect[np.arange(y_true.size), y_true.astype(np.int_)] = 1
-    return (y_pred - pred_vect) / y_true.size
+def sparse_categorical_crossentropy_derivative(y_true, y_pred_logits):
+    n_samples = y_pred_logits.shape[0]
+    y_true_one_hot = np.eye(y_pred_logits.shape[1])[y_true.astype(int)].reshape(y_pred_logits.shape)
+    return (y_pred_logits - y_true_one_hot) / n_samples
 
 
 loss_derivative_mapping = {
     mean_squared_error: mean_squared_error_derivative,
-    categorical_crossentropy: categorical_crossentropy_derivative
+    sparse_categorical_crossentropy: sparse_categorical_crossentropy_derivative
 }
-
-if __name__ == "__main__":
-    y_true = np.array([0, 1, 3])  # One-hot encoded true labels
-    y_pred = np.array([[0.7, 0.2, 0.1], [0.1, 0.8, 0.1], [0.2, 0.2, 0.6]])  # Predictions
-    loss = categorical_crossentropy(y_true, y_pred)
-    print(loss)
-    derivative = categorical_crossentropy_derivative(y_true, y_pred)
-    print(derivative)
